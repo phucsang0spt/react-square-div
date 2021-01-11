8d7b0bd69354bdacadc93890bf6273b72e429066
phucsang <phucsang0spt@gmail.com>
2021-01-11 19:29:35 +0700

    First setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# react-square-div
+# A component to render a square div that have height equal to width
+
+In your App.js or index.js need to import below line:
+`import "react-square-div/lib/styles.css"`.
+
+Now just need to `import SquareDiv from 'react-square-div` and use it in any where you want.
---
+++ index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+interface SquareDivProps {
+  children: React.ReactNode;
+  ratio?: 2 | 1.5 | 1 | 0.7 | 0.6 | 0.5;
+  background?: string;
+}
+
+export default function SquareDiv({
+  background,
+  ratio = 1,
+  children
+}: SquareDivProps) {
+  return (
+    <div
+      className="square-div"
+      style={{ background, paddingTop: `${100 * ratio}%` }}
+    >
+      <div className="square-div_fixed-layer">
+        <div className="square-div_normal-layer">{children}</div>
+      </div>
+    </div>
+  );
+}
